3522f6ce4bdb949bc7aa644daeb0bf3ad5ca7420
heavyrian2012 <heavyrain.lee@wildfirehat.cn>
2022-10-12 11:46:44 +0800

    编译之前先清除编译目录

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+rm -rf build
+
 xcodebuild -target 'yuv.xcodeproj' -scheme 'yuv' -configuration 'Release' -sdk iphoneos CONFIGURATION_BUILD_DIR='build/iphoneos' clean build ONLY_ACTIVE_ARCH=NO PLATFORM_NAME=iphoneos
 
 xcodebuild -target 'yuv.xcodeproj' -scheme 'yuv' -configuration 'Release' -sdk iphonesimulator CONFIGURATION_BUILD_DIR='build/iphonesimulator' clean build ONLY_ACTIVE_ARCH=NO PLATFORM_NAME=iphonesimulator
