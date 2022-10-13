b6fba4f93e317b3468356c8f4e38dd138213144c
Andrey Panchenko <asfdfdfd@asfdfdfd.com>
2022-10-13 14:35:32 +0300

    Fix 'build.sh' to build fat binary.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-xcodebuild -target 'yuv.xcodeproj' -scheme 'yuv' -configuration 'Release' -sdk iphoneos CONFIGURATION_BUILD_DIR='build/iphoneos' clean build ONLY_ACTIVE_ARCH=NO PLATFORM_NAME=iphoneos
+xcodebuild -target 'yuv.xcodeproj' -scheme 'yuv' -configuration 'Release' -sdk iphoneos CONFIGURATION_BUILD_DIR='build/iphoneos' clean build ARCHS='arm64' ONLY_ACTIVE_ARCH=NO PLATFORM_NAME=iphoneos
 
-xcodebuild -target 'yuv.xcodeproj' -scheme 'yuv' -configuration 'Release' -sdk iphonesimulator CONFIGURATION_BUILD_DIR='build/iphonesimulator' clean build ONLY_ACTIVE_ARCH=NO PLATFORM_NAME=iphonesimulator
+xcodebuild -target 'yuv.xcodeproj' -scheme 'yuv' -configuration 'Release' -sdk iphonesimulator CONFIGURATION_BUILD_DIR='build/iphonesimulator' clean build ARCHS='x86_64' ONLY_ACTIVE_ARCH=NO PLATFORM_NAME=iphonesimulator
                      
 lipo -create build/iphoneos/libyuv.a build/iphonesimulator/libyuv.a -output build/libyuv.a
